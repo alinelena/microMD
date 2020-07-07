c808e3e6938c5a1a64261d5884518c60f589b508
Alin Marin Elena <alin@elena.space>
2020-07-07 16:10:02 +0100

    more timing and neighbours

--- source/microMD.F90
+++ source/microMD.F90
@@ -41,12 +41,14 @@ program microMD
   call init_timer_system(tmr, io%uout,3)
   call compilerInfo(io%uout)
   write (io%uout, '(a)') trim(dummy)
+  call start_timer(tmr, 'setup')
   call init_random(control%seed)
   call readConfig(particles, io)
   call particles%summary(io)
   call readField(io, particles)
   call particles%fieldSummary(io)
   call setupNeighbours(particles)
+  call stop_timer(tmr, 'setup')
 
   write (io%uout, '(a,a)') "energy units: ", trim(particles%units)
   call long_range_correction(particles,control%rc)
@@ -54,29 +56,39 @@ program microMD
   block
     integer :: t
     character(len=k_ml) ::  fmte,fmts
+    call start_timer(tmr, 'neighbours')
     call buildNeighbours(particles, control%rc)
+    call stop_timer(tmr, 'neighbours')
 
+    call start_timer(tmr, 'short range')
     call computeForces(particles, control)
+    call stop_timer(tmr, 'short range')
+
+    call particles%init_velocities(control%temperature)
+    call particles%energy()
+    call start_timer(tmr, 'ouput')
     if (io%isTraj) then
       call particles%writeTrajectory(io, 0, 0.0_rp, 0.0_rp, isFirst=.true., level=2)
     endif
     fmte = '('//"i8,1x,5(es13.6,1x)"//')'
     fmts = '('//"a8,1x,5(a13,1x)"//')'
-
     write (io%utimeser, fmt=trim(fmts)) "Timestep","Time", "TotEng", "T", "Epair","E_tail"
-    call particles%init_velocities(control%temperature)
-    call particles%energy()
     write (io%utimeser, fmt=trim(fmte)) 0,control%time, particles%eng / engUnits, particles%temperature, &
       particles%engPair / engUnits, particles%englrc / engUnits
+    call stop_timer(tmr, 'ouput')
     do t = 1, control%steps
       control%time = control%time + control%timestep
       control%step = control%step + 1
+    call start_timer(tmr, 'neighbours')
+    call buildNeighbours(particles, control%rc)
+    call stop_timer(tmr, 'neighbours')
       call nve_update_velocity(particles, control%timestep)
       call nve_update_position(particles, control%timestep)
       call start_timer(tmr, 'short range')
       call computeForces(particles, control)
       call stop_timer(tmr, 'short range')
       call nve_update_velocity(particles, control%timestep)
+      call start_timer(tmr, 'ouput')
       if ( (t==1) .or.(mod(t,control%freq) == 0) ) then
         call particles%energy()
         write (io%utimeser, fmt=trim(fmte)) t,control%time, particles%eng / engUnits, particles%temperature, &
@@ -85,6 +97,7 @@ program microMD
       if (io%isTraj) then
         call particles%writeTrajectory(io, control%step, control%time, control%timestep, isFirst=.false., level=2)
       endif
+      call stop_timer(tmr, 'ouput')
     enddo
     call timer_report(tmr)
   end block
